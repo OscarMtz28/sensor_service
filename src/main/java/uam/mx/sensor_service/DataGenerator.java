c1b266ac500bf5f13c229fc74ed4c925caf428b4
OscarMtz <oscar2000xd@gmail.com>
2025-07-16 14:32:19 -0600

    Clase 16 julio

--- src/main/java/uam/mx/sensor_service/DataGenerator.java
+++ src/main/java/uam/mx/sensor_service/DataGenerator.java
@@ -1,12 +1,35 @@
 package uam.mx.sensor_service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import uam.mx.sensor_service.entities.SensorData;
 
 public class DataGenerator {
     
+    private List<SensorData> list = new ArrayList<>(); 
+    public void add (SensorData value){
+        this.list.add(value);
+    }
+
+    public SensorData modify (int index, SensorData value){
+        return this.list.set(index, value);
+    }
+
+    /**
+     * Modificar en la lista el objeto con el nuevo valor
+     * @param index Indice en la posicion de la lista
+     * @param value El objeto con los valores nuevos
+     * @return Referencia al valor modificado
+     */
+
+     public SensorData getById(int index){
+        return this.list.get(index);
+     }
+
     public SensorData generate(String barrio){
+
         Random ran_temperatura = new Random();
         Random ran_humedad = new Random();
         Random ran_calidadAire = new Random();
@@ -25,5 +48,6 @@ public class DataGenerator {
         SensorData data = gen.generate();
         System.out.println(data);
     }*/
+
 
 }
--- src/main/java/uam/mx/sensor_service/SensorController.java
+++ src/main/java/uam/mx/sensor_service/SensorController.java
@@ -2,6 +2,9 @@ package uam.mx.sensor_service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +13,9 @@ import uam.mx.sensor_service.entities.SensorData;
 @RestController
 @RequestMapping("/sensor") // Formacion de la URL
 
+
 public class SensorController{
+
     @Value ("${BARRIO}")
     private String barrio;
 
@@ -23,4 +28,24 @@ public class SensorController{
         SensorData data = gen.generate(barrio);
         return data;
     }
+
+    @PostMapping("/add")
+    public void setValue(@RequestBody SensorData value){
+        DataGenerator gen = new DataGenerator();
+        gen.add(value);
+    }
+
+    @PostMapping("/modify/{index}")
+    public void modify (@PathVariable ("index") int index, @RequestBody SensorData data){
+        DataGenerator gen = new DataGenerator();
+        SensorData data_org = gen.modify(index, data);
+        data_org = data;
+    }
+
+    @GetMapping("/search/{index}")
+    public SensorData search (@PathVariable ("index") int index){
+        DataGenerator gen = new DataGenerator();
+        return gen.getById(index);
+    }
+
 }
