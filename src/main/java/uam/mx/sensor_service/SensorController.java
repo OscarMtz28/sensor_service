8a945e1cf55276a61c7c729971c037fbbded2e88
OscarMtz <oscar2000xd@gmail.com>
2025-07-18 12:38:26 -0600

    documentacion

--- src/main/java/uam/mx/sensor_service/SensorController.java
+++ src/main/java/uam/mx/sensor_service/SensorController.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import uam.mx.sensor_service.entities.SensorData;
 
 @RestController
 @RequestMapping("/sensor") // Formacion de la URL
-
+@Tag(name= "Sensor service", description="Simula una estacion de monitoreo de parametros climatologicos.")
 
 public class SensorController{
 
@@ -28,14 +35,22 @@ public class SensorController{
         SensorData data = gen.generate(barrio);
         return data;
     }
-
+    @Operation(summary = "Agregar un valor de un sensor externo a nuestro servicio de sensor")
     @PostMapping("/add")
     public void setValue(@RequestBody SensorData value){
         DataGenerator gen = new DataGenerator();
         gen.add(value);
     }
 
+    @Operation(summary="Metodos que nodifican el indice de un sensor")
+    @Parameter(name="Index", description = "Indice de la lectura a modificar")
     @PostMapping("/modify/{index}")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Ejecicion exitosa",content =  {@Content(schema = @Schema(implementation = SensorData.class), mediaType = "aplication/json")}),
+        @ApiResponse(responseCode = "404", description = "No se encontro el indice",content = {@Content(schema = @Schema())}),
+        @ApiResponse(responseCode = "500", description = "Error en el servidor", content = {@Content(schema = @Schema())})
+    })
+
     public void modify (@PathVariable ("index") int index, @RequestBody SensorData data){
         DataGenerator gen = new DataGenerator();
         SensorData data_org = gen.modify(index, data);
